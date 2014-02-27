23917c2dafb89d2c7593da28cf81bc06773fde6c
Zack Marvel <zpmarvel@gmail.com>
2014-02-27 06:09:24 -1000

    Improve efficiency, finish the script.
    I'm satisfied with it. Other than cleaning up some code, which I might
    need to do, it accepts any arguments I want it to, and it is more efficient
    than it was.

--- double-factorial.py
+++ double-factorial.py
@@ -2,29 +2,36 @@
 
 import sys
 
-# if this funtion could accept a list of arguments instead of a single
-# argument it wouldn't have to recalculate the list of odd numbers each
-# iteration
+def double_factorial(args):
+    for n in args:
+        if n % 2 == 0 or n < 1:
+            error = 'Must enter an odd number greater than or equal to one.'
+            raise ValueError(error)
 
-def double_factorial(n):
-    if n % 2 == 0 or n < 1:
-        error = 'Must enter an odd number greater than or equal to one.'
-        raise ValueError(error)
+    max_arg = max(args)
     total = 1
-    for i in [x for x in range(1, n + 1) if x % 2 != 0]:
-        total *= i
-    return total
+    results = []
+    for arg in args:
+        for i in [x for x in range(1, max_arg + 1) if x % 2 != 0]:
+            if i <= arg:
+                total *= i
+        results.append(total)
+        total = 1
+    return results
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if sys.argv[1] == '-n':
-            args = [int(x) for x in sys.argv[2:]]
-            for x in args:
-                for i in range(1, x + 1):
-                    print double_factorial(2 * i - 2)
+            arg = int(sys.argv[2])
+            args = []
+            for i in range(1, arg + 1):
+                args.append(2 * i - 1)
+            results = double_factorial(args)
         else:
             args = [int(x) for x in sys.argv[1:]]
-            for x in args:
-                print double_factorial(x)
+            results = double_factorial(args)
+        for result in results:
+            print result
     else:
         Exception('At least one integer is expected.')
